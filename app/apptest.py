03421931478a92d6cdd4d9bcf4678489a29192e3
chou <choukatanagari@yahoo.com>
2021-06-11 10:09:45 +0800

    worklist

--- app/apptest.py
+++ app/apptest.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ucctransporttopsecret'
 
 
-
+'''
 #live test
 scopes = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 json_creds = os.getenv("GOOGLE_SHEETS_CREDS_JSON")
@@ -30,11 +30,12 @@ scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/aut
 creds = ServiceAccountCredentials.from_json_keyfile_name('app/ucc-transport.json', scope)
 client = gspread.authorize(creds)
 #local test
-'''
+
 
 
 spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1sc9skuA6O9Jh19WaL2N_qAfw6CaabjO9BUMhb8A3-wY")
 sheet = spreadsheet.sheet1 
+
 
 
 @app.route('/', methods=['POST','GET'])
@@ -42,15 +43,22 @@ def index():
 
 
     dict_sheet = sheet.get_all_records()
+    for d in dict_sheet:
+        d['cac'] = d.pop('CAC/PKD')
+        d['pic'] = d.pop('Name PIC/Pemanggil')
+        d['contact'] = d.pop('No Contact PIC/Pemanggil')
+        d['status'] = d.pop('Status UCC')
+
+
     date_times = (datetime.now() + timedelta(hours=8)).strftime("%d/%m/%Y %H:%M:%S")
    
-    
-    cac_name = [d['CAC/PKD'] for d in dict_sheet]
+    '''
+    cac_name = [d['cac'] for d in dict_sheet]
     link_gs = [d['A. Link ke Google Sheet'] for d in dict_sheet]
     file_gs = [d['B. ATAU MuatNaik Line listing'] for d in dict_sheet]
     response_form = [d['Form Response Edit URL'] for d in dict_sheet]
-    ucc_status = [d['Status UCC'] for d in dict_sheet]
-
+    ucc_status = [d['status'] for d in dict_sheet]
+    
 
     def delete_link(link):
         empty = '.'
@@ -80,55 +88,25 @@ def index():
             return siap
         else:
             return belum
-    
-    return render_template('index.html',  
-    dict_sheet = dict_sheet,
-    date_times=date_times,
-    cac_name=cac_name,
-    link_gs=link_gs,
-    file_gs=file_gs,
-    response_form=response_form,
-    ucc_status=ucc_status,
-    delete_link=delete_link,
-    disabled=disabled,
-    tick_icon=tick_icon,
-    delete_edit=delete_edit
-    )
-
-@app.route('/worklist', methods=['POST','GET'])
-def worklist():
-
-
-    dict_sheet = sheet.get_all_records()
-    for d in dict_sheet:
-        d['cac'] = d.pop('CAC/PKD')
-        d['pic'] = d.pop('Name PIC/Pemanggil')
-        d['contact'] = d.pop('No Contact PIC/Pemanggil')
-        d['status'] = d.pop('Status UCC')
-        d['g_sheet'] = d.pop('A. Link ke Google Sheet')
-        d['excel'] = d.pop('B. ATAU MuatNaik Line listing')
-    
-    def delete_link(link):
-        empty = ''
-        text ='LINK'
-        if link == '':
-            return empty
-        else:
-            return text
-
-
-    date_times = (datetime.now() + timedelta(hours=8)).strftime("%d/%m/%Y %H:%M:%S")
-   
+    '''
     status_dict = ['J2', 'J3', 'J4', 'J5', 'J6', 'J7', 'J8', 'J9', 'J10' ]
     uccform = zip(dict_sheet, status_dict)
 
 
-    return render_template('worklist.html',  
+    return render_template('index.html',  
     dict_sheet = dict_sheet,
     date_times=date_times,
     status_dict=status_dict,
-    uccform=uccform,
-    delete_link=delete_link
+    uccform=uccform
+    #cac_name=cac_name,
+    #link_gs=link_gs,
+    #file_gs=file_gs,
+    #response_form=response_form,
+    #ucc_status=ucc_status,
+    #delete_link=delete_link,
+    #disabled=disabled,
+    #tick_icon=tick_icon,
+    #delete_edit=delete_edit
     )
 
 @app.route('/status', methods = ['POST'])
@@ -138,7 +116,7 @@ def status():
     status_id = request.form['status_id']
     sheet.update(status_id, status_form)
     if request.method == 'POST':
-        return redirect(url_for('worklist'))
+        return redirect(url_for('index'))
  
 
 @app.route('/form')
