bc36e8afe8a7b90e949f16837ba03105722baa61
chou <choukatanagari@yahoo.com>
2021-06-25 09:39:12 +0800

    sheet

--- app/app.py
+++ app/app.py
@@ -34,7 +34,7 @@ client = gspread.authorize(creds)
 '''
 
 spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1sc9skuA6O9Jh19WaL2N_qAfw6CaabjO9BUMhb8A3-wY")
-sheet = spreadsheet.sheet1 
+sheet = spreadsheet.sheet3 
 
 
 @app.route('/', methods=['POST','GET'])
